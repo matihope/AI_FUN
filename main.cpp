9971531ea80d769843cf70f873bdfe4ef67135b4
Mateusz Kołpa <mateusz.kolpa@wp.pl>
2023-09-26 18:44:56 +0200

    Change main.cpp to run game

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include "Scenes/MainMenu.hpp"
 
 int main() {
-	IdxDigitTrainer::teachImagesAugmented("digitsAugmented.json");
-	IdxDigitTrainer::testImages("digitsAugmented.json");
-	//	mk::Game &game = mk::Game::get();
-	//	if (!game.init("settings.json")) return 1;
-	//
-	//	game.addScene(std::make_unique<MainMenu>());
-	//
-	//	game.run();
+	//	IdxDigitTrainer::teachImagesAugmented("digitsAugmented.json");
+	//	IdxDigitTrainer::testImages("digitsAugmented.json");
+	mk::Game &game = mk::Game::get();
+	if (!game.init("settings.json")) return 1;
+
+	game.addScene(std::make_unique<MainMenu>());
+
+	game.run();
 }
